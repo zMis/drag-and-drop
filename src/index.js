d0a0ddb5a0c5591a64df6bfba1308889299712dc
Jakub <Jakub@10.7.0.47>
2016-09-06 18:41:41 +0200

    Plne vykreslovani dat

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import DragAndDropApp from './App';
 import './index.css';
+import { Draggable, Droppable } from 'react-drag-and-drop'
 
 ReactDOM.render(
   <DragAndDropApp />,
